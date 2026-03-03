7c4fcad8ae9be326585f930518d499ad2c6f8f3e
Burak Çevik <dvrevb@outlook.com>
2026-03-04 00:16:25 +0300

    add notification.py to send notifications via pushover

---
+++ notification.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+
+def push(text):
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": os.getenv("PUSHOVER_TOKEN"),
+            "user": os.getenv("PUSHOVER_USER"),
+            "message": text,
+        }
+    )
+
